e480d058ba65263938222bc72e3eeafe42589caf
koval <kovalskiy0303@mail.ru>
2024-06-20 17:41:46 +0300

    fixed price when updating title

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ require("dotenv").config({ path: "../.env" });
 
 const webpush = require("web-push");
 
+const multer = require('multer');
+
 const path = require("path");
 // Импортируем Express, фреймворк для создания веб-приложений на Node.js
 const express = require("express");
@@ -84,6 +86,19 @@ const optionsStore = {
 
 // Создание хранилища
 const sessionStore = new MySQLStore(optionsStore);
+
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  }
+});
+
+const upload = multer({ storage: fileStorage });
+
 
 async function syncModels() {
   try {
